4e1dc92a2c88491a7db189bcceb09701de48cfb7
Tobias Weigl <tobias.weigl99@gmail.com>
2020-11-16 16:10:06 +0100

    Revert "Update main.js"
    This reverts commit 4608bdeb

--- static/main.js
+++ static/main.js
@@ -1,39 +1,40 @@
 let baseUrl = window.location.origin;
 var offset = 0;
 
-$(function () {
-    loadTasks();
+$(function (){
+        loadTasks();
 });
 
-function loadTasks() {
+function loadTasks(){
     let config = {
-        'limit': 2,
-        'offset': offset,
+        'limit' : 2,
+        'offset' : offset,
     }
     $.ajax({
         type: "GET",
         url: baseUrl + '/tasks',
         dataType: 'json',
         data: config,
-        success: function (data) {
+        success: function (data){
             var even;
             even = offset % 2 == 0;
             var count = 0;
-            $.each(data.tasks, function (key, value) {
+            $.each(data.tasks, function (key, value){
                 var template = $('.row-template').clone();
                 template.removeClass('row-template');
-                if (even) {
-                    template.addClass('row-even');
-                } else {
+                if(even){
+                  template.addClass('row-even');
+                }
+                else{
                     template.addClass('row-odd');
                 }
                 var templateItems = template.children('td');
                 $(templateItems[0]).html(value.name);
                 $(templateItems[1]).html(value.assignee);
                 var dateString = value.dueByDate;
-                var year = dateString.substr(0, 4);
-                var month = dateString.substr(4, 2);
-                var day = dateString.substr(6, 2);
+                var year = dateString.substr(0,4);
+                var month = dateString.substr(4,2);
+                var day = dateString.substr(6,2);
                 $(templateItems[2]).html(day + '.' + month + '.' + year);
                 var stateColumn = $(templateItems[3]).children('div');
                 $(stateColumn[0]).addClass(value.state);
@@ -43,7 +44,7 @@ function loadTasks() {
                 count++;
             })
             offset += count;
-            if (!data.more) {
+            if(!data.more){
                 $('#more-tasks-button').css('display', 'none');
             }
         }
